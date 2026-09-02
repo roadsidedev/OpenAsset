43a4bec3fb1ab007b61dc0d1f8d8d6fb9fd03127
roadsidedev <1.46009228e+08+roadsidedev@users.noreply.github.com>
2026-09-02 03:09:41 +0000

    docs: integrate developer guides into platform docs

--- web/content/guides/_meta.js
+++ web/content/guides/_meta.js
@@ -1,4 +1,8 @@
 export default {
+  developer: 'Developer guide',
+  'developer-quickstart': 'Developer quick start',
+  'reference-adapters': 'Reference adapters',
+  'developer-feedback': 'Developer feedback',
   'create-market': 'Create a market',
   borrow: 'Borrow against collateral',
   adapters: 'Use adapters',
